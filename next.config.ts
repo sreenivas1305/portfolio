f216098ec3ad6ec8f07f5849096774ed4d799b68
Latha Kumpatla <kumpatlalatha3@gmail.com>
2026-01-22 20:09:37 +0530

    modified url

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
-const nextConfig = {
-  output: 'export',
-  
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
-module.exports = nextConfig;
+export default nextConfig;
